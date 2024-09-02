fbe9a73a4b15fcdbd7600bd96fb3585754af3a98
linggash <sempanalingga@gmail.com>
2024-09-02 11:09:00 +0700

    Learned about Method Overriding

--- src/main/java/com/linggash/spring_validation_learning/helper/SayHello.java
+++ src/main/java/com/linggash/spring_validation_learning/helper/SayHello.java
@@ -2,11 +2,9 @@ package com.linggash.spring_validation_learning.helper;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 
 @Component
-@Validated
-public class SayHello {
+public class SayHello implements ISayHello{
 
     public String sayHello(@NotBlank String name) {
         return "Hello " + name;
