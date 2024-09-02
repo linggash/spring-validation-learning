2f51fa802582da85956f6f594b5eaa97340a44fa
linggash <sempanalingga@gmail.com>
2024-09-02 09:58:01 +0700

    Init Project

---
+++ src/main/java/com/linggash/spring_validation_learning/SpringValidationLearningApplication.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_validation_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringValidationLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linggash/spring_validation_learning/SpringValidationLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_validation_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
