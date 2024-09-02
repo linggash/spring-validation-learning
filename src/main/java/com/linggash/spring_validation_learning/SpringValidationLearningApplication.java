eac8930a59e41fa83f68bc7fe1649d92802a9bf5
linggash <sempanalingga@gmail.com>
2024-09-02 11:25:26 +0700

    Learned about Configuration Properties Validation

--- src/main/java/com/linggash/spring_validation_learning/SpringValidationLearningApplication.java
+++ src/main/java/com/linggash/spring_validation_learning/SpringValidationLearningApplication.java
@@ -1,8 +1,13 @@
 package com.linggash.spring_validation_learning;
 
+import com.linggash.spring_validation_learning.properties.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({
+		DatabaseProperties.class
+})
 @SpringBootApplication
 public class SpringValidationLearningApplication {
 
